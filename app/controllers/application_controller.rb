5c232bf3e67dff67e2922d2cc09f762da9198feb
trose618 <trose618@gmail.com>
2018-12-05 18:59:35 -0500

    create additional session methods for query help. Remove agent from apt form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,11 +9,35 @@ class ApplicationController < ActionController::Base
     !!current_agent
   end
 
+  def log_in!(agent)
+    session[:user_id] = agent.id
+  end
+
+  def setup_agent_stuff
+    if session["user_id"]
+      @logged_in_agent = Agent.find(session["user_id"])
+      setup_agent_arrays
+    end
+  end
+
+  def setup_agent_arrays
+    @agent_apartments = @logged_in_agent.apartments
+    @agent_buildings = @logged_in_agent.buildings
+  end
+
+  def logged_on?
+    !!session[:user_id]
+  end
+
   def require_agent
     if !logged_in?
       flash[:danger] = "You must be logged to perform this"
       redirect_to root_path
     end
+  end
+
+  def authenticate
+    redirect_to new_session_path unless logged_in?
   end
 
 end
